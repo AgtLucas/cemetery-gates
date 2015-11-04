8fc74d79421d6ce8ad0423c975dbba59003712b4
Lucas <lucas@agtlucas.com>
2015-11-04 18:37:43 -0200

    Update server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+import path from 'path';
+import express from 'express';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import config from './webpack.config.js';
+
+const app = express();
+const compiler = webpack(config);
+
+app.use(express.static(__dirname + '/dist'));
+app.use(webpackMiddleware(compiler));
+
+app.get('*', function response(req, res) {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(process.env.PORT || 3000);
