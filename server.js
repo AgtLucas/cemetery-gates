b1bdcfec0fe0e00cd9f7047d8b6258b9709d2606
Lucas <lucas@agtlucas.com>
2015-11-04 18:54:55 -0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import path from 'path';
 import express from 'express';
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
 import config from './webpack.config.js';
 
 const app = express();
@@ -9,6 +10,7 @@ const compiler = webpack(config);
 
 app.use(express.static(__dirname + '/dist'));
 app.use(webpackMiddleware(compiler));
+app.use(webpackHotMiddleware(compiler));
 
 app.get('*', function response(req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
