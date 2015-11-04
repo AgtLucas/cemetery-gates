b180a5628f23c26a1a92e25a97c232116b5edb7b
Lucas <lucas@agtlucas.com>
2015-11-04 18:59:45 -0200

    Update webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+import path from 'path';
+
+const config = {
+
+  devtool: 'eval-source-map',
+
+  entry: [
+    'webpack-hot-middleware/client', path.join(__dirname, 'app/main.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }, {
+      test: /\.css?$/,
+      loader: 'style.css'
+    }]
+  }
+
+};
